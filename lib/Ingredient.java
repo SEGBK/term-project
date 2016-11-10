ba088f00f7f78cfd921be519dc30987072e043a4
Karim Alibhai <karim@alibhai.co>
2016-11-09 20:21:43 -0500

    improve documentation of Ingredient class

--- lib/Ingredient.java
+++ lib/Ingredient.java
@@ -7,14 +7,39 @@ public class Ingredient implements Comparable<Ingredient> {
     private double quantity;
     private String name, units;
 
+    /**
+     * Creates a new Ingredient object from values.
+     */
     public Ingredient(String name, double quantity, String units) {
         this.name = name;
         this.quantity = quantity;
         this.units = units;
     }
 
-    public int compareTo(Ingredient o) { return this.name.compareTo(o.getName()); }
+    /**
+     * Compares the name of this Ingredient object with another.
+     * @param other a different Ingredient object
+     * @return -1, 0, or 1 if the name of this object is less
+     *         than, equal to, or greater than the name of ther oher object
+     */
+    public int compareTo(Ingredient other) { return this.name.compareTo(other.getName()); }
+
+    /**
+     * Retrieves the common name for the ingredient.
+     * @return the name of the ingredient as a string
+     */
     public String getName() { return this.name; }
+
+    /**
+     * Retrieves the quantity of this ingredient required.
+     * @return a scalar representing quantity
+     */
     public double getQuantity() { return this.quantity; }
+
+    /**
+     * Retrieves the units in which quantity should be measured
+     * for this ingredient.
+     * @return the common name for this unit
+     */
     public String getUnits() { return this.units; }
 }
