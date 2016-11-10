6d27fbed1c2fac646fdcac3d04e703c311ece51d
Karim Alibhai <karim@alibhai.co>
2016-11-09 20:22:13 -0500

    move class comment of Saveable so that it is found by javadoc

--- lib/Saveable.java
+++ lib/Saveable.java
@@ -1,17 +1,16 @@
+package lib;
+
 /**
  * Similar to Java's serializable interface, but
  * specific to this project.
  */
-
-package lib;
-
 public abstract class Saveable {
     /**
      * This is the constant by which different datums must
      * be separated in a serialized string.
      */
-    public final static char DELIM = '\0';
-
+    public final static String DELIM = "\0";
+ 
     /**
      * Serializes a Saveable object into a string.
      * @return Saveable in the form of a string (contains unreadable characters)
