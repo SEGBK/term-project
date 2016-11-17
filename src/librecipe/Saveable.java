3859c5c4e3349c754399b810b67af9c1bf06c7a0
Karim Alibhai <karim@alibhai.co>
2016-11-17 00:54:18 -0500

    add exceptions to signatures of Saveable methods (issue #6)

--- src/librecipe/Saveable.java
+++ src/librecipe/Saveable.java
@@ -1,5 +1,9 @@
 package librecipe;
 
+import java.io.IOException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+
 /**
  * Similar to Java's serializable interface, but
  * specific to this project and uses Jackson for
@@ -10,12 +14,12 @@ public abstract class Saveable {
      * Serializes a Saveable object into a string.
      * @return Saveable in the form of a string (contains unreadable characters)
      */
-    public abstract String serialize();
+    public abstract String serialize() throws JsonMappingException, IOException;
 
     /**
      * Deserializes a string into a Saveable object.
      * @param serialized the original serialized object as a string
      * @return the Saveable object for chaining
      */
-    protected abstract Saveable deserialize(String serialized);
+    protected abstract Saveable deserialize(String serialized) throws JsonParseException, IOException;
 }
