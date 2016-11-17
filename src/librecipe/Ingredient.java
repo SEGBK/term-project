61b5f972880e66016c2c107c9f816f445fc88d3d
Karim Alibhai <karim@alibhai.co>
2016-11-17 00:59:02 -0500

    implement Saveable interface inside of Ingredient class (issue #6)

--- src/librecipe/Ingredient.java
+++ src/librecipe/Ingredient.java
@@ -1,9 +1,15 @@
 package librecipe;
 
+import java.io.IOException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+
 /**
  * Stores details on the quantity of an ingredient.
  */
-public class Ingredient implements Comparable<Ingredient> {
+public class Ingredient extends Saveable implements Comparable<Ingredient> {
     private double quantity;
     private String name, units;
 
@@ -15,6 +21,20 @@ public class Ingredient implements Comparable<Ingredient> {
         this.quantity = quantity;
         this.units = units;
     }
+
+    /**
+     * Creates a new Ingredient object from JSON.
+     * @param json the json string to deserialize from 
+     */
+    public Ingredient(String json) throws JsonParseException, IOException {
+        this.deserialize(json);
+    }
+
+    /**
+     * Creates an empty Ingredient object.
+     * WARNING: Only for Jackson use.
+     */
+    public Ingredient() {}
 
     /**
      * Compares the name of this Ingredient object with another.
@@ -42,4 +62,33 @@ public class Ingredient implements Comparable<Ingredient> {
      * @return the common name for this unit
      */
     public String getUnits() { return this.units; }
+
+    /**
+     * Serializes a Saveable object into a string.
+     * @return Ingredient in the form of a string (contains unreadable characters)
+     */
+    public String serialize() throws JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationConfig.Feature.INDENT_OUTPUT);
+        return mapper.writeValueAsString(this);
+    }
+
+    /**
+     * Deserializes a string into a Ingredient object.
+     * @param serialized the original serialized object as a string
+     * @return the Ingredient object for chaining
+     */
+    protected Ingredient deserialize(String serialized) throws JsonParseException, IOException {
+        // deserialize the object
+        ObjectMapper mapper = new ObjectMapper();
+        Ingredient unwrapped = mapper.readValue(serialized, Ingredient.class);
+
+        // copy properties over
+        this.name = unwrapped.getName();
+        this.quantity = unwrapped.getQuantity();
+        this.units = unwrapped.getUnits();
+
+        // return for chaining
+        return this;
+    }
 }
