d6802c92251f8ae42512888a8b2e307704c565fe
Karim Alibhai <karim@alibhai.co>
2016-11-28 20:05:44 -0500

    add some js to pull recipes

---
+++ scripts/allrecipes.js
@@ -0,0 +1,72 @@
+/**
+ * scripts/allrecipes.js - librecipe
+ * Crawler that fetches a collection of recipes from
+ * allrecipes.com.
+ */
+
+'use strict';
+
+const url = require('url')
+    , format = require('util').format
+    , cheerio = require('cheerio')
+    , request = require('request')
+
+module.exports = query => new Promise((resolve, reject) => {
+    const path = format('http://allrecipes.com/search/results/?wt=%s&sort=re', encodeURIComponent(query))
+    request(path, (err, res, body) => {
+        if (err || !res || res.statusCode > 399) {
+            reject(err || ('Something went wrong.' + (res ? ' (code: ' + res.statusCode + ')' : '')))
+        } else {
+            const $_ = cheerio.load(body)
+                , dict = []
+                , rows = []
+
+            // parse single column results for nutritional facts
+            $_('article.grid-col--fixed-tiles a[href*="/recipe/"]').each((i, row) => {
+                let href = $_(row).attr('href')
+                if (rows.indexOf(href) === -1) {
+                    rows.push(href)
+                    /*LIMIT*/if(i>1)return;/*LIMIT*/
+                    dict.push(new Promise((resolve, reject) => {
+                        request(url.resolve(path, href), (err, res, page) => {
+                            if (err || !res || res.statusCode > 399) {
+                                reject(err || ('Something went wrong.' + (res ? ' (code: ' + res.statusCode + ')' : '')))
+                            } else {
+                                const $ = cheerio.load(page)
+                                    , clean = text => text && text !== 'ADVERTISEMENT'
+                                    , convert = {
+                                        'm': m => m
+                                    }
+                                    , time = text => {
+                                        let num = parseFloat(text.find('span').text())
+                                          , unit = text.text().replace(new RegExp('' + num, 'g'), '').trim()
+                                        return convert[unit](num) || [num, unit]
+                                      }
+                                    , obj = {
+                                        name: $('[itemprop="name"]').text(),
+                                        servings: parseFloat($('[ng-model="servings"]').val()),
+                                        prept: time($('[itemprop="prepTime"]')),
+                                        cookt: time($('[itemprop="cookTime"]')),
+                                        ingredients: [],
+                                        steps: []
+                                    }
+
+                                // pull all ingredients
+                                $('[class*="list-ingredients"] li span').each((_, span) => clean($(span).text()) && obj.ingredients.push($(span).text()))
+
+                                // add all steps
+                                $('[itemprop="recipeInstructions"] .step span').each((_, span) => clean($(span).text()) && obj.steps.push($(span).text()))
+
+                                // return obj
+                                console.log(JSON.stringify(obj, null, 2))
+                                resolve(obj)
+                            }
+                        })
+                    }))
+                }
+            })
+
+            Promise.all(dict).then(resolve, reject)
+        }
+    })
+})
---
+++ scripts/search.js
@@ -0,0 +1,73 @@
+/**
+ * scripts/search.js - librecipe
+ * Runs a search against allrecipes.com
+ */
+'use strict';
+
+const query = process.argv.slice(2).join(' ')
+    , LogisticRegressionClassifier = require('natural').LogisticRegressionClassifier
+
+console.log(' * Searching for "%s" ...', query)
+require('./allrecipes')(query).then(recipes => {
+    console.log(JSON.stringify(recipes.map(recipe => {
+        const ingredientClassifier = new LogisticRegressionClassifier()
+
+        // parse ingredients into pairs of measurements and ingredients
+        recipe.ingredients = recipe.ingredients.map((text, index) => {
+            let i = 0
+              , amount = ''
+              , units = ''
+
+            // separate text from amount
+            for (i = 0; i < text.length; i += 1) {
+                if (text[i].match(/[a-z]/i)) {
+                    text = text.substr(i)
+                    text = text.split(/\s+/g)
+
+                    if (text[0].match(/(kg|kilogram|g|gram|pound)(s?)/gi)) {
+                        units = text[0]
+                        text = text.slice(1).join(' ')
+                    }
+
+                    break
+                } else amount += text[i]
+            }
+
+            // reduce amount into a single value
+            amount = amount.split(/\s+/g).reduce((prev, num) => {
+                if (!num) return prev
+                num = num.split('/')
+                return prev + (num.length === 2 ? parseInt(num[0], 10) / parseInt(num[1], 10) : parseFloat(num.join('')))
+            }, 0)
+
+            ingredientClassifier.addDocument(text, String(index))
+
+            return [[amount, units], text]
+        })
+
+        // train classifier
+        ingredientClassifier.train()
+
+        // run classifier over steps to cluster by ingredients
+        let map = {}
+        recipe.steps = recipe.steps.map(text => {
+            let step = {
+                ingredients: [],
+                text: text
+            }
+
+            ingredientClassifier.getClassifications(text).forEach(classification => {
+                if (classification.value > 0.5) {
+                    let ingredient = JSON.parse(JSON.stringify(recipe.ingredients[+classification.label]))
+                    if (map[classification.label]) ingredient[0][0] = 0
+                    map[classification.label] = true
+                    step.ingredients.push(ingredient)
+                }
+            })
+
+            return step
+        })
+
+        return recipe
+    }), null, 2))
+}, console.log)
